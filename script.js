b8af2e2c5ad781f578a4eec6a925674ddd4c353c
luckydixit0464 <luckydixit0464@gmail.com>
2026-08-07 22:21:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -381,3 +381,11 @@ heroImage.style.transform =
 
 }
 );
+function openCertificate(image){
+    document.getElementById("certificateModal").style.display="block";
+    document.getElementById("certificateImage").src=image;
+}
+
+function closeCertificate(){
+    document.getElementById("certificateModal").style.display="none";
+}
